4b93c573cfc3a22fbf4c4d384a1b679a306bbb8e
Stephen F. Booth <sfbooth@earthlink.net>
2014-08-15 09:58:31 +0800

    cgicc-3.0.2

---
+++ cgicc/HTMLClasses.hh
@@ -0,0 +1,34 @@
+/*
+ *  $Id: HTMLClasses.hh,v 1.2 1999/06/04 00:07:37 sbooth Exp $
+ *
+ *  Copyright (C) 1996, 1997, 1998, 1999 Stephen F. Booth
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "cgicc/HTTPHeaders.hh"
+#include "cgicc/HTMLAttributes.hh"
+#include "cgicc/HTMLElements.hh"
+#include "cgicc/HTMLDirLang.hh"
+#include "cgicc/HTMLForms.hh"
+#include "cgicc/HTMLGlobal.hh"
+#include "cgicc/HTMLGraphics.hh"
+#include "cgicc/HTMLLinks.hh"
+#include "cgicc/HTMLLists.hh"
+#include "cgicc/HTMLObjects.hh"
+#include "cgicc/HTMLScripts.hh"
+#include "cgicc/HTMLStyles.hh"
+#include "cgicc/HTMLTables.hh"
+#include "cgicc/HTMLText.hh"
